9da5dd05952e6cab22175cadcc0741432b13b976
woot9697 <ScoutGarrison9697@gmail.com>
2022-08-28 13:04:48 -0700

    Updated the login page and home page with use-effect auth listening and nextjs routing

---
+++ pages/home.js
@@ -0,0 +1,44 @@
+import { TextInput, Box, Group, Divider, Button } from "@mantine/core";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { supabase } from "../lib/initSupabase";
+
+const homePage = () => {
+    const router = useRouter()
+  useEffect(() => {
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      (event, session) => {
+        if (event === "SIGNED_OUT") router.push("/");
+      }
+    );
+    return (
+      () => {
+        authListener.unsubscribe();
+      },
+      []
+    );
+  });
+  return (
+    <Group
+      sx={{
+        flexDirection: "column",
+        width: "100%",
+        backgroundColor: "white",
+        padding: "1rem",
+      }}
+    >
+      <TextInput placeholder="Lobby Code" label="Join Existing Lobby" />
+      <Divider />
+      <Button
+        variant="gradient"
+        gradient={{ from: "indigo", to: "cyan" }}
+        onClick={() => createLobby()}
+      >
+        Create Lobby
+      </Button>
+      <Button onClick={() => supabase.auth.signOut()}>LogOut</Button>
+    </Group>
+  );
+};
+
+export default homePage;
--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,45 @@
-import Link from 'next/link'
-import useSWR from 'swr'
-import { Auth, Card, Typography, Space, Button, Icon } from '@supabase/ui'
-import { supabase } from '../lib/initSupabase'
-import { useEffect, useState } from 'react'
+import Link from "next/link";
+import { useRouter } from "next/router";
+import useSWR from "swr";
+import { Auth, Card, Typography, Space, Button, Icon } from "@supabase/ui";
+import { supabase } from "../lib/initSupabase";
+import { useEffect, useState } from "react";
 
 const fetcher = (url, token) =>
   fetch(url, {
-    method: 'GET',
-    headers: new Headers({ 'Content-Type': 'application/json', token }),
-    credentials: 'same-origin',
-  }).then((res) => res.json())
+    method: "GET",
+    headers: new Headers({ "Content-Type": "application/json", token }),
+    credentials: "same-origin",
+  }).then((res) => res.json());
 
 const Index = () => {
-  const { user, session } = Auth.useUser()
+  const router = useRouter();
+  const { user, session } = Auth.useUser();
   const { data, error } = useSWR(
-    session ? ['/api/getUser', session.access_token] : null,
+    session ? ["/api/getUser", session.access_token] : null,
     fetcher
-  )
-  const [authView, setAuthView] = useState('sign_in')
+  );
+  const [authView, setAuthView] = useState("sign_in");
 
   useEffect(() => {
     const { data: authListener } = supabase.auth.onAuthStateChange(
       (event, session) => {
-        if (event === 'PASSWORD_RECOVERY') setAuthView('update_password')
-        if (event === 'USER_UPDATED')
-          setTimeout(() => setAuthView('sign_in'), 1000)
-        // Send session to /api/auth route to set the auth cookie.
+        if (event === "PASSWORD_RECOVERY") setAuthView("update_password");
+        if (event === "SIGNED_IN") router.push("/home");
         // NOTE: this is only needed if you're doing SSR (getServerSideProps)!
-        fetch('/api/auth', {
-          method: 'POST',
-          headers: new Headers({ 'Content-Type': 'application/json' }),
-          credentials: 'same-origin',
-          body: JSON.stringify({ event, session }),
-        }).then((res) => res.json())
+        // fetch('/api/auth', {
+        //   method: 'POST',
+        //   headers: new Headers({ 'Content-Type': 'application/json' }),
+        //   credentials: 'same-origin',
+        //   body: JSON.stringify({ event, session }),
+        // }).then((res) => res.json())
       }
-    )
+    );
 
     return () => {
-      authListener.unsubscribe()
-    }
-  }, [])
+      authListener.unsubscribe();
+    };
+  }, []);
 
   const View = () => {
     if (!user)
@@ -50,9 +50,7 @@ const Index = () => {
               src="https://app.supabase.io/img/supabase-dark.svg"
               width="96"
             />
-            <Typography.Title level={3}>
-              Welcome to DallE Fun Auth
-            </Typography.Title>
+            <Typography.Title level={3}>Welcome to DallE Fun</Typography.Title>
           </div>
           <Auth
             supabaseClient={supabase}
@@ -62,60 +60,24 @@ const Index = () => {
             socialButtonSize="xlarge"
           />
         </Space>
-      )
+      );
 
     return (
       <Space direction="vertical" size={6}>
-        {authView === 'update_password' && (
+        {authView === "update_password" && (
           <Auth.UpdatePassword supabaseClient={supabase} />
         )}
-        {user && (
-          <>
-            <Typography.Text>You're signed in</Typography.Text>
-            <Typography.Text strong>Email: {user.email}</Typography.Text>
-
-            <Button
-              icon={<Icon type="LogOut" />}
-              type="outline"
-              onClick={() => supabase.auth.signOut()}
-            >
-              Log out
-            </Button>
-            {error && (
-              <Typography.Text danger>Failed to fetch user!</Typography.Text>
-            )}
-            {data && !error ? (
-              <>
-                <Typography.Text type="success">
-                  User data retrieved server-side (in API route):
-                </Typography.Text>
-
-                <Typography.Text>
-                  <pre>{JSON.stringify(data, null, 2)}</pre>
-                </Typography.Text>
-              </>
-            ) : (
-              <div>Loading...</div>
-            )}
-
-            <Typography.Text>
-              <Link href="/profile">
-                <a>SSR example with getServerSideProps</a>
-              </Link>
-            </Typography.Text>
-          </>
-        )}
       </Space>
-    )
-  }
+    );
+  };
 
   return (
-    <div style={{ maxWidth: '420px', margin: '96px auto' }}>
+    <div style={{ maxWidth: "420px", margin: "96px auto" }}>
       <Card>
         <View />
       </Card>
     </div>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
